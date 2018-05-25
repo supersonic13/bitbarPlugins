5d8eb988fbd1b44a0e24539efe2b0813488d0c3a
Arun Veluri <arun.veluri@bt.com>
2018-05-25 16:42:38 +0100

    Added some text

--- 101excuses.30s.sh
+++ 101excuses.30s.sh
@@ -2,6 +2,6 @@
 
 CONTENT=$(curl -Ls http://developerexcuses.com | grep "text-decoration" | sed 's/<\/*[^>]*>//g')
 
-echo "$CONTENT"
+echo "Excuse #$RANDOM:$CONTENT"
 echo "---"
 echo "Refresh... | refresh=true"
